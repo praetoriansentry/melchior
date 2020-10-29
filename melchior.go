f62f479f9ec9c57ff8928890f8dcd8fd9ee32f2b
John Hilliard <jhilliard@nextjump.com>
2020-10-29 01:06:55 -0400

    some of the latest changes

--- melchior.go
+++ melchior.go
@@ -1,3 +1,4 @@
+// Package is the full implementation of a gemini protocol server
 package main
 
 import (
@@ -18,10 +19,52 @@ const (
 	Version = "0.0.1"
 )
 
+var (
+	MelchiorTLSCert  = ""
+	MelchiorTLSKey   = ""
+	MelchiorHostname = ""
+	MelchiorBindAddr = ""
+	MelchiorRootDir  = ""
+)
+
+func initVars() error {
+	MelchiorTLSCert = os.Getenv("MELCHIOR_TLS_CERT")
+	MelchiorTLSKey = os.Getenv("MELCHIOR_TLS_KEY")
+	MelchiorHostname = os.Getenv("MELCHIOR_HOSTNAME")
+	MelchiorBindAddr = os.Getenv("MELCHIOR_BIND_ADDR")
+	MelchiorRootDir = os.Getenv("MELCHIOR_ROOT_DIR")
+
+	if MelchiorTLSCert == "" || MelchiorTLSKey == "" {
+		return fmt.Errorf("MELCHIOR_TLS_KEY and MELCHIOR_TLS_CERT must both be provided")
+	}
+
+	// Defaults
+	if MelchiorHostname == "" {
+		MelchiorHostname = "localhost"
+	}
+	if MelchiorBindAddr == "" {
+		MelchiorBindAddr = "127.0.0.1:1965"
+	}
+	if MelchiorRootDir == "" {
+		MelchiorRootDir = "."
+	}
+
+	log.Printf("The value of %s is %s", "MELCHIOR_TLS_CERT", MelchiorTLSCert)
+	log.Printf("The value of %s is %s", "MELCHIOR_TLS_KEY", MelchiorTLSKey)
+	log.Printf("The value of %s is %s", "MELCHIOR_HOSTNAME", MelchiorHostname)
+	log.Printf("The value of %s is %s", "MELCHIOR_BIND_ADDR", MelchiorBindAddr)
+	log.Printf("The value of %s is %s", "MELCHIOR_ROOT_DIR", MelchiorRootDir)
+
+	return nil
+}
 func main() {
 	log.Printf("Starting Melchoir Version %s", Version)
 
-	cert, err := tls.LoadX509KeyPair(os.Getenv("MELCHIOR_TLS_CERT"), os.Getenv("MELCHIOR_TLS_KEY"))
+	err := initVars()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cert, err := tls.LoadX509KeyPair(MelchiorTLSCert, MelchiorTLSKey)
 	if err != nil {
 		log.Fatalf("Unable to load TLS certficate: %s", err)
 	}
@@ -29,10 +72,10 @@ func main() {
 	tlsConfig := &tls.Config{
 		Certificates: []tls.Certificate{cert},
 		ClientAuth:   tls.VerifyClientCertIfGiven,
-		ServerName:   os.Getenv("MELCHIOR_HOSTNAME"),
+		ServerName:   MelchiorHostname,
 	}
 
-	listener, err := tls.Listen("tcp", os.Getenv("MELCHIOR_BIND_ADDR"), tlsConfig)
+	listener, err := tls.Listen("tcp", MelchiorBindAddr, tlsConfig)
 	if err != nil {
 		log.Fatalf("Unable to listen: %s", err)
 	}
@@ -89,7 +132,7 @@ func handle(conn net.Conn) error {
 		uPath = uPath + "index.gmi"
 	}
 
-	var root http.Dir = http.Dir(os.Getenv("MELCHIOR_ROOT_DIR"))
+	var root http.Dir = http.Dir(MelchiorRootDir)
 
 	cleanPath := filepath.Clean(uPath)
 	log.Printf("Attempting to open file: %s", cleanPath)

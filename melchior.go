2daed19f0bfd9c708e9ee4629a758482a7c76107
John Hilliard <jhilliard@nextjump.com>
2020-10-29 22:52:07 -0400

    some changes

--- melchior.go
+++ melchior.go
@@ -25,6 +25,7 @@ var (
 	MelchiorHostname = ""
 	MelchiorBindAddr = ""
 	MelchiorRootDir  = ""
+	MelchiorDeadline = 5
 )
 
 func initVars() error {
@@ -33,6 +34,7 @@ func initVars() error {
 	MelchiorHostname = os.Getenv("MELCHIOR_HOSTNAME")
 	MelchiorBindAddr = os.Getenv("MELCHIOR_BIND_ADDR")
 	MelchiorRootDir = os.Getenv("MELCHIOR_ROOT_DIR")
+	tmpMelchiorDeadline := os.Getenv("MELCHIOR_DEADLINE")
 
 	if MelchiorTLSCert == "" || MelchiorTLSKey == "" {
 		return fmt.Errorf("MELCHIOR_TLS_KEY and MELCHIOR_TLS_CERT must both be provided")
@@ -48,22 +50,33 @@ func initVars() error {
 	if MelchiorRootDir == "" {
 		MelchiorRootDir = "."
 	}
+	if tmpMelchiorDeadline != "" {
+		dl, err := strconv.Atoi(tmpMelchiorDeadline)
+		if err == nil && dl > 0 {
+			MelchiorDeadline = dl
+		} else {
+			log.Println("Unable to set deadline. Double check MELCHIOR_DEADLINE")
+		}
+	}
 
 	log.Printf("The value of %s is %s", "MELCHIOR_TLS_CERT", MelchiorTLSCert)
 	log.Printf("The value of %s is %s", "MELCHIOR_TLS_KEY", MelchiorTLSKey)
 	log.Printf("The value of %s is %s", "MELCHIOR_HOSTNAME", MelchiorHostname)
 	log.Printf("The value of %s is %s", "MELCHIOR_BIND_ADDR", MelchiorBindAddr)
 	log.Printf("The value of %s is %s", "MELCHIOR_ROOT_DIR", MelchiorRootDir)
+	log.Printf("The value of %s is %s", "MELCHIOR_DEADLINE", MelchiorDeadline)
 
 	return nil
 }
+
 func main() {
 	log.Printf("Starting Melchoir Version %s", Version)
 
 	err := initVars()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to initialized %s", err)
 	}
+
 	cert, err := tls.LoadX509KeyPair(MelchiorTLSCert, MelchiorTLSKey)
 	if err != nil {
 		log.Fatalf("Unable to load TLS certficate: %s", err)
@@ -79,6 +92,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to listen: %s", err)
 	}
+
 	log.Println("Started listening")
 	defer listener.Close()
 	for {
@@ -87,33 +101,35 @@ func main() {
 			log.Printf("There was an error accepting the connection %s", err)
 		}
 
-		// todo make this configuratable
 		err = conn.SetDeadline(time.Now().Add(5 * time.Second))
 		if err != nil {
 			log.Printf("Unable to set connection deadline: %s", err)
 		}
 
-		err = handle(conn)
-		if err != nil {
-			log.Printf("There was an error handling the connection: %s", err)
-		}
+		go func () {
+			err = handle(conn)
+			if err != nil {
+				log.Printf("There was an error handling the connection: %s", err)
+			}
 
-		err = conn.Close()
-		if err != nil {
-			log.Printf("There was an error closing the connection: %s", err)
-		}
+			err = conn.Close()
+			if err != nil {
+				log.Printf("There was an error closing the connection: %s", err)
+			}
+		}()
 	}
 }
 
 func handle(conn net.Conn) error {
 	log.Printf("Handling connection from %s connecting to us on %s", conn.RemoteAddr(), conn.LocalAddr())
+	// The max length of one of these lines should be 1024 characters + \r\n
 	requestData := make([]byte, 1026)
 	readLen, err := conn.Read(requestData)
 	if err != nil {
 		return err
 	}
+
 	inputURL := requestData[0:readLen]
-	log.Println(string(inputURL))
 
 	urlString := strings.TrimSpace(string(inputURL))
 	u, err := url.Parse(urlString)
@@ -122,16 +138,21 @@ func handle(conn net.Conn) error {
 		return err
 	}
 
+	log.Printf("Got a request to %s", urlString)
+
 	uPath := u.Path
 
 	if !strings.HasPrefix(uPath, "/") {
 		uPath = "/" + uPath
 	}
 
+	// If we get a request to a folder, we're going to insert a call to index.gmi as the default file
 	if strings.HasSuffix(uPath, "/") {
 		uPath = uPath + "index.gmi"
 	}
 
+	// Even though we're not using HTTP we can leverage this
+	// function to avoid directory traversal type attacks.
 	var root http.Dir = http.Dir(MelchiorRootDir)
 
 	cleanPath := filepath.Clean(uPath)
